1721b41796c28b80161ed3ed2c10bfb098a5b656
Mario Roberto <mrgomez.dev@gmail.com>
2024-12-03 11:01:32 -0600

    Se agrego email a los campos

--- api/models.py
+++ api/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+import re
+
+
+# Validación personalizada para el campo email
+def validate_email_format(value):
+    """
+    Valida que el valor contenga un @ seguido de un . más adelante.
+    """
+    pattern = r'^[^@]+@[^@]+\.[^@]+$'  # Regex que asegura que exista un @ seguido de un punto
+    if not re.match(pattern, value):
+        raise ValidationError("El correo debe contener un '@' seguido de un '.' en un formato válido.")
+
 
 class Usuario(models.Model):
     nombres = models.CharField(max_length=100)
@@ -8,9 +21,14 @@ class Usuario(models.Model):
     estado_civil = models.CharField(max_length=50)
     fecha_estatus_civil = models.DateField()
     foto_perfil = models.ImageField(upload_to='profile_pics/')
+    email = models.CharField(
+        max_length=100,
+        validators=[validate_email_format],  # Se aplica la validación personalizada
+    )
 
     def __str__(self):
         return f"{self.nombres} {self.apellidos}"
+
 
 class Gasto(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='gastos')
@@ -21,4 +39,3 @@ class Gasto(models.Model):
 
     def __str__(self):
         return f"{self.nombre} - {self.monto}"
-
